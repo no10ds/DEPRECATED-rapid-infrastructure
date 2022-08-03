5985a31cb7368c1ac0a99004c265dff718beb415
Lydiaade <lydia.adejumo@thoughtworks.com>
2022-08-03 11:25:02 +0100

    313-JP/LA Fix protected permission generation using ssm adapter

--- modules/auth/db.tf
+++ modules/auth/db.tf
@@ -34,33 +34,32 @@ resource "aws_dynamodb_table_item" "protected_permissions" {
   hash_key   = aws_dynamodb_table.permissions_table.hash_key
   range_key  = aws_dynamodb_table.permissions_table.range_key
 
-  for_each = toset([
-  for value in nonsensitive(jsondecode(aws_ssm_parameter.protected_domain_permissions.value)) : {
-    permission_name = value.PermissionName
-    type            = value.Type
-    sensitivity     = value.Sensitivity
-    domain          = value.Domain
-  }])
-  item     = <<ITEM
-        {
-          "PK": {"S": "PERMISSION"},
-          "SK": {"S": "${each.value.permission_name}"},
-          "Id": {"S": "${each.value.permission_name}"},
-          "Type": {"S": "${each.value.type}"},
-          "Sensitivity": {"S": "${each.value.sensitivity}"}
-          "Domain": {"S": "${each.value.domain}"}
-        }
-      ITEM
+  for_each = local.protected_permission
+  item     = each.value
+}
+
+locals {
+  protected_permission = toset([
+  for value in nonsensitive(jsondecode(aws_ssm_parameter.protected_domain_permissions.value)) :
+  jsonencode({
+    "PK" : { "S" : "PERMISSION" },
+    "SK" : { "S" : value.PermissionName },
+    "Id" : { "S" : value.PermissionName },
+    "Type" : { "S" : value.Type },
+    "Sensitivity" : { "S" : value.Sensitivity }
+    "Domain" : { "S" : value.Domain }
+  })
+  ])
 }
 
 resource "aws_dynamodb_table_item" "data_permissions" {
-table_name = aws_dynamodb_table.permissions_table.name
-hash_key = aws_dynamodb_table.permissions_table.hash_key
-range_key = aws_dynamodb_table.permissions_table.range_key
+  table_name = aws_dynamodb_table.permissions_table.name
+  hash_key   = aws_dynamodb_table.permissions_table.hash_key
+  range_key  = aws_dynamodb_table.permissions_table.range_key
 
-for_each = var.data_permissions
+  for_each = var.data_permissions
 
-item = <<ITEM
+  item = <<ITEM
       {
         "PK": {"S": "PERMISSION"},
         "SK": {"S": "${each.key}"},
@@ -72,13 +71,13 @@ item = <<ITEM
 }
 
 resource "aws_dynamodb_table_item" "admin_permissions" {
-table_name = aws_dynamodb_table.permissions_table.name
-hash_key = aws_dynamodb_table.permissions_table.hash_key
-range_key = aws_dynamodb_table.permissions_table.range_key
+  table_name = aws_dynamodb_table.permissions_table.name
+  hash_key   = aws_dynamodb_table.permissions_table.hash_key
+  range_key  = aws_dynamodb_table.permissions_table.range_key
 
-for_each = var.admin_permissions
+  for_each = var.admin_permissions
 
-item = <<ITEM
+  item = <<ITEM
       {
         "PK": {"S": "PERMISSION"},
         "SK": {"S": "${each.key}"},
@@ -89,11 +88,11 @@ item = <<ITEM
 }
 
 resource "aws_dynamodb_table_item" "test_client_permissions" {
-table_name = aws_dynamodb_table.permissions_table.name
-hash_key = aws_dynamodb_table.permissions_table.hash_key
-range_key = aws_dynamodb_table.permissions_table.range_key
+  table_name = aws_dynamodb_table.permissions_table.name
+  hash_key   = aws_dynamodb_table.permissions_table.hash_key
+  range_key  = aws_dynamodb_table.permissions_table.range_key
 
-item = <<ITEM
+  item = <<ITEM
   {
     "PK": {"S": "SUBJECT"},
     "SK": {"S": "${aws_cognito_user_pool_client.test_client.id}"},
@@ -105,130 +104,130 @@ item = <<ITEM
 }
 
 data "aws_iam_policy_document" "db_access_logs_key_policy" {
-statement {
-sid = "Enable IAM User Permissions"
+  statement {
+    sid = "Enable IAM User Permissions"
 
-effect = "Allow"
+    effect = "Allow"
 
-principals {
-type = "AWS"
-identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-}
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
 
-actions = [
-"kms:*",
-]
+    actions = [
+      "kms:*",
+    ]
 
-resources = [
-"*",
-]
-}
+    resources = [
+      "*",
+    ]
+  }
 
-statement {
-sid = "Enable Logs Encryption Permissions"
+  statement {
+    sid = "Enable Logs Encryption Permissions"
 
-effect = "Allow"
+    effect = "Allow"
 
-principals {
-type = "Service"
-identifiers = ["logs.${data.aws_region.region.name}.amazonaws.com"]
-}
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.region.name}.amazonaws.com"]
+    }
 
-actions = [
-"kms:Encrypt*",
-"kms:Decrypt*",
-"kms:ReEncrypt*",
-"kms:GenerateDataKey*",
-"kms:Describe*",
-]
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*",
+    ]
 
-resources = [
-"*",
-]
+    resources = [
+      "*",
+    ]
 
-condition {
-test = "ArnEquals"
-variable = "kms:EncryptionContext:aws:logs:arn"
-values = [
-"arn:aws:logs:${data.aws_region.region.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.resource-name-prefix}_${var.permissions_table_name}_access_logs"
-]
-}
-}
+    condition {
+      test     = "ArnEquals"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = [
+        "arn:aws:logs:${data.aws_region.region.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.resource-name-prefix}_${var.permissions_table_name}_access_logs"
+      ]
+    }
+  }
 
-statement {
-sid = "Enable CloudTrail Encryption Permissions"
+  statement {
+    sid = "Enable CloudTrail Encryption Permissions"
 
-effect = "Allow"
+    effect = "Allow"
 
-principals {
-type = "Service"
-identifiers = ["cloudtrail.amazonaws.com"]
-}
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
 
-actions = [
-"kms:Encrypt*",
-"kms:Decrypt*",
-"kms:ReEncrypt*",
-"kms:GenerateDataKey*",
-"kms:Describe*",
-]
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*",
+    ]
 
-resources = [
-"*",
-]
-}
+    resources = [
+      "*",
+    ]
+  }
 }
 
 resource "aws_kms_key" "db_access_logs_key" {
-description = "This key is used to encrypt log objects"
-policy = data.aws_iam_policy_document.db_access_logs_key_policy.json
-tags = var.tags
+  description = "This key is used to encrypt log objects"
+  policy      = data.aws_iam_policy_document.db_access_logs_key_policy.json
+  tags        = var.tags
 }
 
 resource "aws_cloudwatch_log_group" "db_access_logs_log_group" {
-depends_on = [aws_kms_key.db_access_logs_key]
-name = "${aws_dynamodb_table.permissions_table.name}_access_logs"
-retention_in_days = 90
-kms_key_id = aws_kms_key.db_access_logs_key.arn
-tags = var.tags
+  depends_on        = [aws_kms_key.db_access_logs_key]
+  name              = "${aws_dynamodb_table.permissions_table.name}_access_logs"
+  retention_in_days = 90
+  kms_key_id        = aws_kms_key.db_access_logs_key.arn
+  tags              = var.tags
 }
 
 resource "aws_cloudtrail" "db_access_logs_trail" {
-depends_on = [
-aws_s3_bucket_policy.db_access_logs_bucket_policy,
-aws_kms_key.db_access_logs_key
-]
+  depends_on = [
+    aws_s3_bucket_policy.db_access_logs_bucket_policy,
+    aws_kms_key.db_access_logs_key
+  ]
 
-name = "${var.resource-name-prefix}-permissions-table-access-logs"
-s3_bucket_name = aws_s3_bucket.db_access_logs.id
-s3_key_prefix = "permissions-db-access-logs"
-kms_key_id = aws_kms_key.db_access_logs_key.arn
+  name           = "${var.resource-name-prefix}-permissions-table-access-logs"
+  s3_bucket_name = aws_s3_bucket.db_access_logs.id
+  s3_key_prefix  = "permissions-db-access-logs"
+  kms_key_id     = aws_kms_key.db_access_logs_key.arn
 
-is_multi_region_trail = true
-enable_log_file_validation = true
-include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  include_global_service_events = true
 
-event_selector {
-include_management_events = false
-data_resource {
-type = "AWS::DynamoDB::Table"
-values = [
-aws_dynamodb_table.permissions_table.arn
-]
-}
-}
+  event_selector {
+    include_management_events = false
+    data_resource {
+      type   = "AWS::DynamoDB::Table"
+      values = [
+        aws_dynamodb_table.permissions_table.arn
+      ]
+    }
+  }
 
-# CloudTrail requires the Log Stream wildcard
-cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.db_access_logs_log_group.arn}:*"
-cloud_watch_logs_role_arn = aws_iam_role.cloud_trail_role.arn
+  # CloudTrail requires the Log Stream wildcard
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.db_access_logs_log_group.arn}:*"
+  cloud_watch_logs_role_arn  = aws_iam_role.cloud_trail_role.arn
 
-tags = var.tags
+  tags = var.tags
 }
 
 resource "aws_iam_role" "cloud_trail_role" {
-name = "${var.resource-name-prefix}-cloudtrail-cloudwatch-role"
+  name = "${var.resource-name-prefix}-cloudtrail-cloudwatch-role"
 
-assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -243,14 +242,14 @@ assume_role_policy = <<EOF
 }
 EOF
 
-tags = var.tags
+  tags = var.tags
 }
 
 resource "aws_iam_role_policy" "aws_iam_role_policy_cloudtrail_cloudwatch" {
-name = "${var.resource-name-prefix}-cloudtrail-cloudwatch-policy"
-role = aws_iam_role.cloud_trail_role.id
+  name = "${var.resource-name-prefix}-cloudtrail-cloudwatch-policy"
+  role = aws_iam_role.cloud_trail_role.id
 
-policy = <<EOF
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -280,14 +279,14 @@ EOF
 }
 
 resource "aws_s3_bucket" "db_access_logs" {
-bucket = "${var.resource-name-prefix}-permissions-table-access-logs"
-force_destroy = true
-tags = var.tags
+  bucket        = "${var.resource-name-prefix}-permissions-table-access-logs"
+  force_destroy = true
+  tags          = var.tags
 }
 
 resource "aws_s3_bucket_policy" "db_access_logs_bucket_policy" {
-bucket = aws_s3_bucket.db_access_logs.id
-policy = <<POLICY
+  bucket = aws_s3_bucket.db_access_logs.id
+  policy = <<POLICY
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -320,27 +319,27 @@ POLICY
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "db_access_logs_lifecycle" {
-bucket = aws_s3_bucket.db_access_logs.id
+  bucket = aws_s3_bucket.db_access_logs.id
 
-rule {
-id = "expire-old-logs"
-status = "Enabled"
+  rule {
+    id     = "expire-old-logs"
+    status = "Enabled"
 
-expiration {
-days = 90
-}
+    expiration {
+      days = 90
+    }
 
-}
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "db_access_logs_s3_encryption_config" {
-depends_on = [aws_kms_key.db_access_logs_key]
-bucket = aws_s3_bucket.db_access_logs.bucket
+  depends_on = [aws_kms_key.db_access_logs_key]
+  bucket     = aws_s3_bucket.db_access_logs.bucket
 
-rule {
-apply_server_side_encryption_by_default {
-kms_master_key_id = aws_kms_key.db_access_logs_key.arn
-sse_algorithm = "aws:kms"
-}
-}
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.db_access_logs_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
