7c56cb3e7f6abbead3c9ff484e389065e8d32e20
Toby Drane <tdrane@no10.gov.uk>
2022-12-07 11:01:30 +0000

    change permissions

--- modules/app-cluster/db.tf
+++ modules/app-cluster/db.tf
@@ -87,7 +87,8 @@ data "aws_iam_policy_document" "db_access_logs_key_policy" {
       test     = "ArnEquals"
       variable = "kms:EncryptionContext:aws:logs:arn"
       values = [
-        "arn:aws:logs:${data.aws_region.region.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.resource-name-prefix}_service_table_access_logs"
+        "arn:aws:logs:${data.aws_region.region.name}:${data.aws_caller_identity.current.account_id}:log-group:db_access_logs",
+        "arn:aws:logs:${data.aws_region.region.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.resource-name-prefix}_${var.permissions_table}_access_logs"
       ]
     }
   }
