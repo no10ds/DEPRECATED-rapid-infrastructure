4d3b3e62b12d6d9ce964fffe7046ef24595aeea5
Toby Drane <tdrane@no10.gov.uk>
2022-12-22 17:11:06 +0000

    Deploy static to cloudfront

--- blocks/ui/main.tf
+++ blocks/ui/main.tf
@@ -13,11 +13,13 @@ data "terraform_remote_state" "s3-state" {
   }
 }
 
-resource "aws_s3_bucket" "rapid_ui_release_storage" {
+resource "aws_cloudfront_origin_access_identity" "rapid_ui" {}
+
+resource "aws_s3_bucket" "rapid_ui" {
   #checkov:skip=CKV_AWS_144:No need for cross region replication
   #checkov:skip=CKV_AWS_145:No need for non default key
 
-  bucket        = "ui-f1-registry"
+  bucket        = var.ui_information.bucket_name
   force_destroy = true
   tags          = var.tags
 
@@ -26,44 +28,123 @@ resource "aws_s3_bucket" "rapid_ui_release_storage" {
   }
 
   logging {
-    target_bucket = aws_s3_bucket.logs.bucket
+    target_bucket = data.terraform_remote_state.s3-state.outputs.log_bucket_name
     target_prefix = "log/ui-f1-registry"
   }
 }
 
-resource "aws_s3_bucket_acl" "rapid_ui_release_storage_acl" {
-  bucket = aws_s3_bucket.rapid_ui_release_storage.id
-  acl    = "public-read"
+resource "aws_s3_bucket_acl" "rapid_ui_storage_acl" {
+  bucket = aws_s3_bucket.rapid_ui.id
+  acl    = "private"
 }
 
-resource "aws_s3_bucket" "logs" {
-  #checkov:skip=CKV_AWS_144:No need for cross region replication
-  #checkov:skip=CKV_AWS_145:No need for non default key
-  #checkov:skip=CKV_AWS_18:Log bucket shouldn't be logging
-  #checkov:skip=CKV_AWS_21:No need to version log bucket
-  bucket        = "ui-f1-registry-logs"
-  force_destroy = false
-  tags          = var.tags
+resource "aws_s3_bucket_website_configuration" "rapid_ui_website" {
+  bucket = aws_s3_bucket.rapid_ui.bucket
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "" # use default
-        sse_algorithm     = "AES256"
-      }
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+resource "null_resource" "upload_static_ui" {
+  depends_on = [
+    aws_s3_bucket.rapid_ui
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+      url="${var.ui_information.ui_registry_url}/${var.ui_information.ui_version}.zip"
+      # curl -LO $url
+      unzip ${var.ui_information.ui_version}.zip
+      cd out/
+      sed -i '' 's/"REACT_APP_API_URL":"https:\/\/getrapid.link"/"REACT_APP_API_URL":"${var.ui_information.rapid_api_url}"/g' __ENV.js
+      aws s3 cp . s3://${aws_s3_bucket.rapid_ui.id} --recursive
+    EOF
+  }
+}
+
+data "aws_iam_policy_document" "cloudfront" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.rapid_ui.arn}/*",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        aws_cloudfront_origin_access_identity.rapid_ui.iam_arn,
+      ]
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.rapid_ui.arn,
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        aws_cloudfront_origin_access_identity.rapid_ui.iam_arn,
+      ]
     }
   }
 }
 
-resource "aws_s3_bucket_acl" "logs_acl" {
-  bucket = aws_s3_bucket.logs.id
-  acl    = "private"
+resource "aws_s3_bucket_policy" "cloudfront" {
+  bucket = aws_s3_bucket.rapid_ui.id
+  policy = data.aws_iam_policy_document.cloudfront.json
 }
 
-resource "aws_s3_bucket_public_access_block" "logs" {
-  bucket                  = aws_s3_bucket.logs.id
-  ignore_public_acls      = true
-  block_public_acls       = true
-  block_public_policy     = true
-  restrict_public_buckets = true
+resource "aws_cloudfront_distribution" "rapid_ui" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.rapid_ui.bucket_regional_domain_name
+    origin_id   = "${var.resource-name-prefix}-ui-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.rapid_ui.cloudfront_access_identity_path
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["GB"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "${var.resource-name-prefix}-ui-origin"
+    viewer_protocol_policy = "allow-all"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
 }
--- blocks/ui/variables.tf
+++ blocks/ui/variables.tf
@@ -8,7 +8,30 @@ variable "state_bucket" {
   description = "Bucket name for backend state"
 }
 
+variable "resource-name-prefix" {
+  type        = string
+  description = "The prefix to add to resources for easier identification"
+}
+
 variable "aws_account" {
   type        = string
   description = "AWS Account number to host the rAPId service"
+}
+
+variable "ui_information" {
+  type = object({
+    ui_registry_url = string,
+    ui_version      = string,
+    bucket_name     = string,
+    rapid_api_url   = string # Do we want these another object for env variables?
+  })
+
+  description = <<EOT
+    ui_information = {
+      ui_registry_url: "Url location for the built static ui"
+      ui_version: "Version number for the built ui static files (e.g.: v5.0)"
+      bucket_name: "The name of the S3 bucket that hosts the static ui files"
+      rapid_api_url: "The url of the rapid instance that the ui will request"
+    }
+  EOT
 }
