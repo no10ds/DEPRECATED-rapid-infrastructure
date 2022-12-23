6885bc5685bf93f5b85bc09e9021fdc7285b0fba
Ricky Parkinson <2026055+rickyp72@users.noreply.github.com>
2022-12-23 15:55:22 +0000

    add e2e clients data and user admins

--- modules/auth/main.tf
+++ modules/auth/main.tf
@@ -104,3 +104,59 @@ resource "aws_secretsmanager_secret_version" "user_login_client_secrets_version"
     client_secret = aws_cognito_user_pool_client.user_login.client_secret
   })
 }
+
+# E2E_TEST_CLIENT_USER_ADMIN
+resource "aws_cognito_user_pool_client" "e2e_test_client_user_admin" {
+  name = "${var.resource-name-prefix}_e2e_test_client_user_admin"
+
+  user_pool_id        = aws_cognito_user_pool.rapid_user_pool.id
+  generate_secret     = true
+  explicit_auth_flows = var.rapid_client_explicit_auth_flows
+  allowed_oauth_scopes = [
+    "${aws_cognito_resource_server.rapid_resource_server.identifier}/CLIENT_APP",
+  ]
+  allowed_oauth_flows                  = ["client_credentials"]
+  allowed_oauth_flows_user_pool_client = true
+}
+
+resource "aws_secretsmanager_secret" "e2e_test_client_user_admin" {
+  # checkov:skip=CKV_AWS_149:AWS Managed Key is sufficient
+  name = "${var.resource-name-prefix}_E2E_TEST_CLIENT_USER_ADMIN"
+}
+
+resource "aws_secretsmanager_secret_version" "e2e_test_client_user_admin_secrets_version" {
+  secret_id = aws_secretsmanager_secret.e2e_test_client_user_admin.id
+  secret_string = jsonencode({
+    client_name   = aws_cognito_user_pool_client.e2e_test_client_user_admin.name
+    client_id     = aws_cognito_user_pool_client.e2e_test_client_user_admin.id
+    client_secret = aws_cognito_user_pool_client.e2e_test_client_user_admin.client_secret
+  })
+}
+
+# E2E_TEST_CLIENT_DATA_ADMIN
+resource "aws_cognito_user_pool_client" "e2e_test_client_data_admin" {
+  name = "${var.resource-name-prefix}_e2e_test_client_data_admin"
+
+  user_pool_id        = aws_cognito_user_pool.rapid_user_pool.id
+  generate_secret     = true
+  explicit_auth_flows = var.rapid_client_explicit_auth_flows
+  allowed_oauth_scopes = [
+    "${aws_cognito_resource_server.rapid_resource_server.identifier}/CLIENT_APP",
+  ]
+  allowed_oauth_flows                  = ["client_credentials"]
+  allowed_oauth_flows_user_pool_client = true
+}
+
+resource "aws_secretsmanager_secret" "e2e_test_client_data_admin" {
+  # checkov:skip=CKV_AWS_149:AWS Managed Key is sufficient
+  name = "${var.resource-name-prefix}_E2E_TEST_CLIENT_DATA_ADMIN"
+}
+
+resource "aws_secretsmanager_secret_version" "e2e_test_client_data_admin_secrets_version" {
+  secret_id = aws_secretsmanager_secret.e2e_test_client_data_admin.id
+  secret_string = jsonencode({
+    client_name   = aws_cognito_user_pool_client.e2e_test_client_data_admin.name
+    client_id     = aws_cognito_user_pool_client.e2e_test_client_data_admin.id
+    client_secret = aws_cognito_user_pool_client.e2e_test_client_data_admin.client_secret
+  })
+}
