a81880c4bcc352357a466a5433321fe2e18744b8
Ricky Parkinson <2026055+rickyp72@users.noreply.github.com>
2023-01-05 12:45:51 +0000

    add db permissions tables

--- modules/auth/main.tf
+++ modules/auth/main.tf
@@ -127,9 +127,9 @@ resource "aws_secretsmanager_secret" "e2e_test_client_user_admin" {
 resource "aws_secretsmanager_secret_version" "e2e_test_client_user_admin_secrets_version" {
   secret_id = aws_secretsmanager_secret.e2e_test_client_user_admin.id
   secret_string = jsonencode({
-    client_name   = aws_cognito_user_pool_client.e2e_test_client_user_admin.name
-    client_id     = aws_cognito_user_pool_client.e2e_test_client_user_admin.id
-    client_secret = aws_cognito_user_pool_client.e2e_test_client_user_admin.client_secret
+    CLIENT_NAME   = aws_cognito_user_pool_client.e2e_test_client_user_admin.name
+    CLIENT_ID     = aws_cognito_user_pool_client.e2e_test_client_user_admin.id
+    CLIENT_SECRET = aws_cognito_user_pool_client.e2e_test_client_user_admin.client_secret
   })
 }
 
@@ -155,8 +155,64 @@ resource "aws_secretsmanager_secret" "e2e_test_client_data_admin" {
 resource "aws_secretsmanager_secret_version" "e2e_test_client_data_admin_secrets_version" {
   secret_id = aws_secretsmanager_secret.e2e_test_client_data_admin.id
   secret_string = jsonencode({
-    client_name   = aws_cognito_user_pool_client.e2e_test_client_data_admin.name
-    client_id     = aws_cognito_user_pool_client.e2e_test_client_data_admin.id
-    client_secret = aws_cognito_user_pool_client.e2e_test_client_data_admin.client_secret
+    CLIENT_NAME   = aws_cognito_user_pool_client.e2e_test_client_data_admin.name
+    CLIENT_ID     = aws_cognito_user_pool_client.e2e_test_client_data_admin.id
+    CLIENT_SECRET = aws_cognito_user_pool_client.e2e_test_client_data_admin.client_secret
+  })
+}
+
+## E2E_TEST_CLIENT_READ_ALL_WRITE_ALL
+resource "aws_cognito_user_pool_client" "e2e_test_client_read_and_write" {
+  name = "${var.resource-name-prefix}_e2e_test_client_read_and_write"
+
+  user_pool_id        = aws_cognito_user_pool.rapid_user_pool.id
+  generate_secret     = true
+  explicit_auth_flows = var.rapid_client_explicit_auth_flows
+  allowed_oauth_scopes = [
+    "${aws_cognito_resource_server.rapid_resource_server.identifier}/CLIENT_APP",
+  ]
+  allowed_oauth_flows                  = ["client_credentials"]
+  allowed_oauth_flows_user_pool_client = true
+}
+
+resource "aws_secretsmanager_secret" "e2e_test_client_read_and_write" {
+  # checkov:skip=CKV_AWS_149:AWS Managed Key is sufficient
+  name = "${var.resource-name-prefix}_E2E_TEST_CLIENT_READ_ALL_WRITE_ALL_placeholder"
+}
+
+resource "aws_secretsmanager_secret_version" "e2e_test_client_read_and_write_secrets_version" {
+  secret_id = aws_secretsmanager_secret.e2e_test_client_read_and_write.id
+  secret_string = jsonencode({
+    CLIENT_NAME   = aws_cognito_user_pool_client.e2e_test_client_read_and_write.name
+    CLIENT_ID     = aws_cognito_user_pool_client.e2e_test_client_read_and_write.id
+    CLIENT_SECRET = aws_cognito_user_pool_client.e2e_test_client_read_and_write.client_secret
+  })
+}
+
+# E2E_TEST_CLIENT_WRITE_ALL
+resource "aws_cognito_user_pool_client" "e2e_test_client_write_all" {
+  name = "${var.resource-name-prefix}_e2e_test_client_write_all"
+
+  user_pool_id        = aws_cognito_user_pool.rapid_user_pool.id
+  generate_secret     = true
+  explicit_auth_flows = var.rapid_client_explicit_auth_flows
+  allowed_oauth_scopes = [
+    "${aws_cognito_resource_server.rapid_resource_server.identifier}/CLIENT_APP",
+  ]
+  allowed_oauth_flows                  = ["client_credentials"]
+  allowed_oauth_flows_user_pool_client = true
+}
+
+resource "aws_secretsmanager_secret" "e2e_test_client_write_all" {
+  # checkov:skip=CKV_AWS_149:AWS Managed Key is sufficient
+  name = "${var.resource-name-prefix}_E2E_TEST_CLIENT_WRITE_ALL"
+}
+
+resource "aws_secretsmanager_secret_version" "e2e_test_client_write_all_secrets_version" {
+  secret_id = aws_secretsmanager_secret.e2e_test_client_write_all.id
+  secret_string = jsonencode({
+    CLIENT_NAME   = aws_cognito_user_pool_client.e2e_test_client_write_all.name
+    CLIENT_ID     = aws_cognito_user_pool_client.e2e_test_client_write_all.id
+    CLIENT_SECRET = aws_cognito_user_pool_client.e2e_test_client_write_all.client_secret
   })
 }
