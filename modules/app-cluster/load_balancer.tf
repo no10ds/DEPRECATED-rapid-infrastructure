2c601ae7929f5edd31ddf73dcf5d3560abe2acfb
Ricky Parkinson <2026055+rickyp72@users.noreply.github.com>
2023-01-24 13:19:25 +0000

    pentest result 2.9
    prevent ALB from listening on port 80

--- modules/app-cluster/load_balancer.tf
+++ modules/app-cluster/load_balancer.tf
@@ -62,14 +62,6 @@ POLICY
 resource "aws_security_group" "load_balancer_security_group" {
   vpc_id      = var.vpc_id
   description = "ALB Security Group"
-#  ingress {
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = var.ip_whitelist
-#    description = "Allow HTTP ingress"
-#  }
-
   ingress {
     from_port   = 443
     to_port     = 443
